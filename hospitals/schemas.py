6a8d4e96405134163a640fc8f5b00025f79ecd56
JoseDev7 <josefr7h@gmail.com>
2025-10-28 07:50:50 -0300

    Creacion de la clase HospitalFilterSchema

--- hospitals/schemas.py
+++ hospitals/schemas.py
@@ -1,4 +1,5 @@
-from ninja import ModelSchema , FilterSchema
+from ninja import ModelSchema , FilterSchema, Field
+from typing import Optional
 from .models import Hospital
 
 # Schema class of pydantics used to validate data reques (validar datos de entrada), and format data of response(validar datos de salida )
@@ -12,4 +13,7 @@ class HospitalSchema(ModelSchema):
         model_fields = '__all__'
 
 
-#schema para busqueda 
+#schema para busqueda segun la ciudad y el nombre del hospital
+class HospitalFilterSchema(FilterSchema):
+    city_town: Optional[str] = None
+    hospital_name: Optional[str] = None
